f0c8459e9632e13d05ce5d1d3507d7edabfc22d0
CelsoLucas <lucascelso23@gmail.com>
2024-09-24 16:48:23 -0400

    Soma dos dois primeiros valores multiplicado pelo terceiro valor

---
+++ ex5.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+n3 = float(input("Digite o terceiro valor: "))
+
+tot = (n1 + n2) * n3
+
+print(f"O resultado da soma dos dois primeiros valores multiplicado pelo terceiro é {tot}")
