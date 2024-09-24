e0e14611d1be136845233d4f81923424753c4240
CelsoLucas <lucascelso23@gmail.com>
2024-09-24 15:12:28 -0400

    Atualização conversor de Real para Dolar

--- ex3.py
+++ ex3.py
@@ -1,4 +1,4 @@
 real = float(input("Digite o valor que você tem em reais: R$"))
 dolar = float(input("Digite o valor do dolar: $"))
-
-print(f"R${real} convertido para dolar é ${real/dolar:.2f}")
+conver = real / dolar
+print(f"R${real} convertido para dolar é ${conver:.2f}")
