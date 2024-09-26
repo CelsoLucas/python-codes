970384822daf10298ced0cc119038af509be46e8
CelsoLucas <lucascelso23@gmail.com>
2024-09-26 16:11:26 -0400

    Aula de manipulação de strings

---
+++ manipulacaodestrings.py
@@ -0,0 +1,63 @@
+# a = "Celso"
+# print(len(a))
+# mostra a quantidade de caracteres da variavel
+
+# a = "celso"
+# print(a.capitalize())
+# Transforma a primeira letra em maiuscula
+
+# a = "celso"
+# print(a.upper())
+# transforma todo texto em maiusculo
+
+# a = "CELSO"
+# print(a.casefold())
+# transforma todo texto em minusculo
+
+# a = "CELSO"
+# print(a.lower())
+#transforma todo texto em minusculo
+
+# a = "CELSO"
+# print(a.islower())
+# verifica se o texto é todo minusculo
+
+# a = "celso"
+# print(a.isupper())
+# verifica se o texto é todo maiusculo
+
+# a = "12345"
+# print(a.isdigit())
+# Verifica se a string só possui números inteiros
+
+# a = "Celso Lucas"
+# print(a.replace("Lucas", "Primon"))
+# troca a palavra indicada primeiro pela segunda palavra indicada
+
+# a = "Celso Lucas Primon"
+# print(a.split(" "))
+# separa a string usando sep como separador
+
+# a = "Celso Lucas Primon"
+# print(a.find("L"))
+# mostra o indice do que você coloca dentro parametro
+
+# a = "Celso Lucas Primon"
+# print("Primon" in a)
+# verifica se o texto indicado esta na string
+
+# a = "Celso Lucas Primon"
+# print(a.count("o"))
+#conta quantas vezes o parametro indicado aparece na string
+
+# a = "Olá, Mundo!"
+# print(a[0])
+# print(a[2])
+# print(a[6])
+# mostra a letra do texto no indice que você indicar
+
+# a = "Olá, Mundo!"
+# print(a[1:3])
+#mostra os caracteres que estão nos indices indicados, *o segundo parametro é o limitador ele não aparece*
+
+
