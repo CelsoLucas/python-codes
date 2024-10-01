0c8c37913f200a9c1ba02efb6368db664738316e
CelsoLucas <lucascelso23@gmail.com>
2024-10-01 17:27:16 -0400

    lista com 5 valores, print maior, menor, soma e quantidade de valores

---
+++ ex15.py
@@ -0,0 +1,5 @@
+lista = [1, 2, 3, 4, 5]
+print(f"O maoir numero é {max(lista)}")
+print(f"O menor numero é {min(lista)}")
+print(f"A lista tem {len(lista)} elementos")
+print(f"A soma dos valores da lista é {sum(lista)}")
