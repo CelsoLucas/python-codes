50961e1dc7b7ba95a312d5079464a26562136114
CelsoLucas <lucascelso23@gmail.com>
2024-10-01 17:23:26 -0400

    Aula Listas

---
+++ aulalistas.py
@@ -0,0 +1,48 @@
+# lista = ["Ederson", 37, 1.75, 78]
+# print(lista)
+#Uma lista é uma sequencia ou coleção orientada
+
+# lista1 = [10, 20, 30, 40]
+# lista2 = ["Sapm", "Bungee", "Swallow"]
+# print(lista1, lista2)
+
+# lista1 = ["oi", 2.0, 5, [10, 20]]
+# print(lista1)
+#lista aninhada(uma lista dentro de outra)
+
+# lista1 = ["oi", 2.0, 5, [10, 20]]
+# print(len(lista1))
+#A função len não conta os elementos que estão dentro da sublista ele conta a sub lista como um elemento só
+
+# numeros = [17, 123, 87, 34, 66, 8393, 44]
+# print(numeros[2])
+# print(numeros[9-8])
+# print(numeros[-2])
+# print(numeros[-1])
+#o parametro de indices em uma lista pode ser usado para mostrar o item no indice indicado da lista
+
+# frutas = ["maça", "laranja", "banana", "cereja"]
+# print("maça" in frutas)
+# print("pera" in frutas)
+#in serve para fazer pesquisa, nesse caso ele pesquisa se tem a fruta indicada e retorna verdadeiro ou falso
+
+# frutas = ["maça", "laranja", "banana", "cereja"]
+# print([1,2] + [3, 4])
+# print(frutas + [6, 7, 8, 9])
+# print(["teste"] * 4)
+# print([1, 2, ["ola", "adeus"]] * 2)
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(a)
+# print(min(a)) #pega o menor valor
+# print(max(a)) #pega o maior valor
+# print(sum(a)) #soma
+
+# lista = ["a", "b", "c", "d", "e", "f"]
+# print(lista[1:3])
+# print(lista[:4])
+# print(lista[3:])
+# print(lista[:])
+# print(lista[0:6])
+
+
