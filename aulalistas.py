7cba36019fbb8b56aa7292ae002f9a0f178fea12
CelsoLucas <lucascelso23@gmail.com>
2024-10-14 16:16:48 -0400

    atualização mais funções

--- aulalistas.py
+++ aulalistas.py
@@ -45,4 +45,56 @@
 # print(lista[:])
 # print(lista[0:6])
 
+# frutas = ["banana", "maça", "cereja"]
+# frutas[0] = "pera"
+# frutas[-1] = "laranja"
+# print(frutas)
+
+# lista = ["a", "b", "c", "d", "e", "f"]
+# lista[1:3] = ["x", "y"]
+# print(lista)
+
+# lista = ["a", "b", "c", "d", "e", "f"]
+# print(lista)
+# print(len(lista))
+# lista[1:3] = []
+# print(lista)
+# print(len(lista))
+# remover item de uma lista 
+
+# lista = ["a", "d", "f"]
+# lista[1:1] = ["b", "c"]
+# print(lista)
+# lista[4:4] = ["e"]
+# print(lista)
+#inserir elementos em uma lista espremendo em uma fatia vazia na posição desejada
+
+# a = [81, 42, 66]
+# a.append(5)
+# print(a)
+
+# a = [88, 44, 76, 22]
+# a.sort()
+# print(a)
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(a.index(5))
+
+# a = [55, 66, 33, 77, 88]
+# a.insert(1, 100)
+# print(a)
+
+# a = [88, 33, 88, 11, 88, 99, 00]
+# print(a)
+# print(a.count(88))
+
+# a = [66, 22, 787, 22, 663, 99, 55]
+# a.pop()
+# print(a)
+# a.pop(0)
+# print(a)
+
+# lista = [1, 2, 3]
+# lista.extend(4, 5)
+# print(lista)
 
