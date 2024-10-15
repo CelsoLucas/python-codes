ab89718199dad07645914138568a3cbb02d13626
CelsoLucas <lucascelso23@gmail.com>
2024-10-15 13:45:51 -0400

    Aula Tuplas

---
+++ aulatuplas.py
@@ -0,0 +1,28 @@
+# got = ("Game of Thrones", 2011, 2019, 9.4)
+# print(got)
+# print(type(got))
+# print(len(got))
+#declaração implicita
+
+# got = tuple(["Game of Thrones", 2011, 2019, 9.4])
+# print(got)
+# print(type(got))
+# print(len(got))
+#declaração explicita
+
+# empresas = ("Google", "Facebook", "Amazon")
+# print(empresas[2])
+
+# empresas = ("Google", "Facebook", "Amazon")
+# print(empresas[0:3])
+
+# top5 = ("Black Mirror", "Bracking Bad", "Friends", "Game of Thrones", "The Big Bang Therory")
+# print("House" in top5)
+
+# empresas = (1, 2, 3, 4, 5)
+# empresas1 = (6, 7, 8, 9, 10)
+# print(empresas + empresas1)
+
+# empresas = ("Brasil", "EUA", "Canada", "Russia")
+# print(empresas.index("Russia"))
+
