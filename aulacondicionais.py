00750b4d80199c75e1434adca967f7bff8b319ab
CelsoLucas <lucascelso23@gmail.com>
2024-10-15 13:50:03 -0400

    aula condicionais

---
+++ aulacondicionais.py
@@ -0,0 +1,9 @@
+# idade = 18
+# if idade < 20:
+#     print("Você é jovem!")
+
+# idade = int(input("Digite sua idade: "))
+# if idade > 18:
+#     print("Maior de idade")
+# else:
+#     print("Menor de idade")
