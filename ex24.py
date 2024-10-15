deaa93b63bd8fbc7d9d8cbcdceb16f7108b039d0
CelsoLucas <lucascelso23@gmail.com>
2024-10-15 14:11:37 -0400

    ver se passou com a media

--- ex24.py
+++ ex24.py
@@ -1,6 +1,9 @@
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
-media = (n1 + n2) / 2
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4
 if media == 10:
     print(f"Sua média foi {media} Aprovado com distinção")
 elif media >= 7:
