fb082a82c3083d0f6a232488e2c9a1f01a66be15
CelsoLucas <lucascelso23@gmail.com>
2024-10-15 14:04:29 -0400

    ver se foi aprovado com a media

--- ex24.py
+++ ex24.py
@@ -2,8 +2,8 @@ n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 media = (n1 + n2) / 2
 if media == 10:
-    print("Aprovado com distinção")
+    print(f"Sua média foi {media} Aprovado com distinção")
 elif media >= 7:
-    print("Você foi aprovado!")
+    print(f"Sua média foi {media} Você foi aprovado!")
 else:
-    print("Você foi reprovado!")
+    print(f"Sua média foi {media} Você foi reprovado!")
