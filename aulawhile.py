00e39a9ac02a29a96640497ed1060fca0a745cf4
CelsoLucas <lucascelso23@gmail.com>
2024-10-17 14:32:45 -0400

    Aula while

---
+++ aulawhile.py
@@ -0,0 +1,22 @@
+# while True:
+#     print("Loop Infinito\n")
+#laço infinito
+
+# while True:
+#     valor = int(input("Digite 1 ou 0 Para sair: "))
+#     if valor == 1:
+#         print("Valor correto")
+#     elif valor == 0:
+#         print("Valor para sair")
+#         break
+#laço infinito até o parametro
+
+# while True:
+#     valor = int(input("Digite 1 ou 0 Para sair: "))
+#     if valor >= 1:
+#         continue
+#         print("Maior que 1")
+#     elif valor >= 1:
+#         print("Maior que 1")
+#continue usa para voltar ao começo do laço e não mostra as outras entradas ou saidas de dados se estiverem depois dele
+
---
+++ ex33.py
@@ -0,0 +1,8 @@
+cont = 100
+c = 0
+while cont >= 0:
+    print(cont, end=" ")
+    cont -= 1
+    c += 1
+print("\n")
+print(f"Para chegar a 0 seu sistema reduziu o valor {c} vezes")
