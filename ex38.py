d282da9913a6e04a47e6e3dcba21671f99dd756e
CelsoLucas <lucascelso23@gmail.com>
2024-10-22 15:14:15 -0300

    anos para a passar b

--- ex38.py
+++ ex38.py
@@ -1,13 +1,11 @@
 a = 80000
 b = 200000
 cont = 0
-while True:
-    if a <= b:
-        a += (a * 0.03)
-        b += (b * 0.015)
-        cont += 1
-    else:
-        break
+while a <= b:
+    a += (a * 0.03)
+    b += (b * 0.015)
+    cont += 1
+
 print(f"A: {a}")
 print(f"B: {b}")
 print(f"Para o A passar o B ele levou {cont} anos")
