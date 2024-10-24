349043d3c9fd0c21092b47511e8b146d3fcf7772
CelsoLucas <lucascelso23@gmail.com>
2024-10-24 16:03:19 -0300

    aula for

---
+++ aulafor.py
@@ -0,0 +1,32 @@
+# nomes = ["Celso", "Ederson", "Anne"]
+# for n in nomes:
+#     print(n)
+
+# nome = "Celso"
+# for n in nome:
+#     print(n, end="")
+
+# nomes = ["Celso", "Ederson", "Anne"]
+# for n in nomes:
+#     print(n)
+#     if n == "Ederson":
+#         break
+
+# nomes = ["Celso", "Ederson", "Anne"]
+# for n in nomes:
+#     if n == "Ederson":
+#         continue
+#     print(n)
+
+# for x in range(6):
+#     print(x)
+
+# for x in range(2, 6):
+#     print(x)
+
+# for x in range(2, 10, 2):
+#     print(x)
+
+for i in range(5):
+    for j in range(6):
+        print(i, j)
