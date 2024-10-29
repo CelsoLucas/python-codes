c8769ff9f3065acc22a54d770990ddaafd44b44c
CelsoLucas <lucascelso23@gmail.com>
2024-10-29 18:17:43 -0300

    fibonacci usando for

--- ex49.py
+++ ex49.py
@@ -3,7 +3,7 @@ n = int(input("Digite um numero: "))
 t1 = 0
 t2 = 1
 print(f"{t1} -> {t2}", end="")
-for f in range(2, n+1):
+for f in range(n - 2):
     t3 = t1 + t2
     print(f" -> {t3}", end="")
     t1 = t2
