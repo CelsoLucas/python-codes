537a52105636c2cdf9d340c05b79b19911f92c04
CelsoLucas <lucascelso23@gmail.com>
2024-10-29 17:27:19 -0300

    Media de 5 numeros usando for

--- ex48.py
+++ ex48.py
@@ -3,4 +3,4 @@ for c in range(1, 6):
     num = float(input(f"Número {c}: "))
     soma += num
 med = soma / 5
-print(f"A média das notas é {med}")
+print(f"A soma dos números é {soma} e a média das notas é {med}")
