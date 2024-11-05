d68f987c3a76215bb78c6af7c589f9d88eb53200
CelsoLucas <lucascelso23@gmail.com>
2024-11-05 16:08:26 -0300

    Aula Dicionario

---
+++ auladicionario.py
@@ -0,0 +1,68 @@
+# tradutor = {}
+# tradutor["pineapple"] = "Abacaxi"
+# tradutor["apple"] = "Maça"
+# tradutor["orange"] = "Laranja"
+# print(type(tradutor))
+# print(tradutor)
+
+# tradutor1 = {}
+# tradutor1 = {"pinapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(type(tradutor1))
+# print(tradutor1)
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor1["apple"])
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor1)
+# del(tradutor1["apple"])
+# print(tradutor1)
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor1)
+# print(tradutor1.pop("pineapple", "Fruta não encontrada"))
+# print(tradutor1)
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor1)
+# tradutor1.clear()
+# print(tradutor1)
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print("pineapple" in tradutor1)
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print("pineapple" in tradutor1.values())
+# print(tradutor1.values())
+
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor1)
+# tradutor1["apple"] = "goiaba"
+# print(tradutor1)
+
+# dados = {
+#         "Crossfox":{"km":35000, "ano":2005},
+#         "DS5":{"km":17000, "ano":2015}, 
+#         "Fusca":{"km":130000, "ano":1979}, 
+#         "Jetta":{"km":56000, "ano":2011}, 
+#         "Passat":{"km":62000, "ano":1999}
+#         }
+# print(dados)
+
+# dados = {
+#         "Crossfox":{"km":35000, "ano":2005},
+#         "DS5":{"km":17000, "ano":2015}, 
+#         "Fusca":{"km":130000, "ano":1979}, 
+#         "Jetta":{"km":56000, "ano":2011}, 
+#         "Passat":{"km":62000, "ano":1999}
+#         }
+# print(dados.get("Jetta", "Veiculo não encontrado"))
+
+
