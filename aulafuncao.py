1eb49055eff5b5ea889994c2d6738a082a9d93f9
CelsoLucas <lucascelso23@gmail.com>
2024-11-12 15:09:19 -0300

    Aula funcao

---
+++ aulafuncao.py
@@ -0,0 +1,32 @@
+# def hello():
+#     print("Olá!!!")
+# hello()
+
+# def hello(nome):
+#     print(f"Olá {nome}")
+# hello("Celso")
+
+# def hello(nome):
+#     print(f"Olá {nome}")
+# nome = str(input("Nome: "))
+# hello(nome)
+
+# def hello(nome, idade):
+#     print(f"Olá {nome} \nVocê tem {idade} anos")
+# hello("Celso", 17)
+
+# def calcular_pagamento(qtd_hrs, valor_hora):
+#     horas = float(qtd_hrs)
+#     taxa = float(valor_hora)
+#     if horas <= 40:
+#         salario = horas * taxa
+#     else:
+#         h_excd = horas - 40
+#         salario = 40 * taxa + (h_excd*(1.5 * taxa))
+#     print(salario)
+
+# qtd_hrs = float(input("Quantidade de horas: "))
+# valor_hora = float(input("Valor hora: "))
+# calcular_pagamento(qtd_hrs, valor_hora)
+
+
