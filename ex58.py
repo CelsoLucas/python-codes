1bbb909d10890ecbb84d9d0116d87c1765ce1ada
CelsoLucas <lucascelso23@gmail.com>
2024-11-25 16:57:47 -0300

    commit
    commit

--- ex58.py
+++ ex58.py
@@ -4,4 +4,20 @@ class ContaCorrente():
         self.nome_correntista = nome_correntista
         self.saldo = saldo
 
-    
+    def alterar_nome(self):
+        self.nome_correntista = str(input("Novo nome: "))
+
+    def deposito(self):
+        self.dep = float(input("Quanto você deseja depositar: R$"))
+        self.saldo = self.saldo + self.dep
+
+    def saque(self):
+        self.saque = float(input("Quanto você deseja sacar: R$"))
+        self.saldo = self.saldo - self.saque
+
+conta1 = ContaCorrente(12345, "Celso", 1000)
+conta1.alterar_nome()
+conta1.deposito()
+conta1.saque()
+print(conta1.nome_correntista)
+print(f"R${conta1.saldo}")
